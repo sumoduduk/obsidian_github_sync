afeaafde95828dd038e28b7c2683ccef250a9755
sumoduduk <sumoduduk@example.com>
2024-01-22 18:43:48 +0700

    feat: add get path function to check if valid folder

---
+++ src/path_fn.rs
@@ -0,0 +1,33 @@
+use eyre::OptionExt;
+use std::path::PathBuf;
+
+pub fn get_path(path_str: &str) -> eyre::Result<PathBuf> {
+    let path = PathBuf::from(path_str);
+    path.is_dir()
+        .then_some(|p: PathBuf| p)
+        .ok_or_eyre("folder not exists")?;
+    Ok(path)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use dotenvy::dotenv;
+    use std::env::{args, var};
+
+    #[test]
+    fn test_path_is_exist() -> eyre::Result<()> {
+        dotenv().ok();
+        let path_str = var("PATH_FOLDER").expect("ERROR: PATH_FOLDER environment must present");
+
+        let path = get_path(&path_str)?;
+
+        let goal = "/home";
+        let parent = path.parent().unwrap();
+        let display = format!("{}", parent.display());
+
+        assert_eq!(goal, &display);
+
+        Ok(())
+    }
+}
