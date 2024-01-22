5116cb5ae04529fbdf007d811f0efef2f5982003
sumoduduk <sumoduduk@example.com>
2024-01-22 18:44:12 +0700

    Todo : finishing this repo

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 mod get_config;
 mod get_time;
 mod git_operation;
+mod path_fn;
+
+use std::env::args;
+use std::process::Command;
 
 use get_time::fetch_current_time;
+// use git_opertaion::{git_add_commit, git_push};
+use path_fn::get_path;
 
-fn main() {
-    todo!()
+fn main() -> eyre::Result<()> {
+    todo!();
+    // let mut args = args();
+    // args.next();
+    //
+    // let path_str = args.next().expect("ERROR: specify folder path on argument");
+    // let path = get_path(&path)?;
+    // let time_str = fetch_current_time()?;
+    //
+    // let mut cmd_add_commit = Command::new("git");
+    //
+    // let output_add_commit = git_add_commit(cmd_add_commit, &time_str, &path);
 }
